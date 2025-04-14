09185831d34ba2227d7855b31600b669f637c963
Cherprang <CherprangA@users.noreply.github.com>
2025-04-14 20:02:31 +0900

    Updating test code

--- test.py
+++ test.py
@@ -1,31 +1,30 @@
 import gym
 import numpy as np
 
-env = gym.make("MountainCar-v0", render_mode="human")
+env = gym.make("MountainCar-v0")
 env.reset()
 
-print(env.observation_space.high)
-print(env.observation_space.low)
-print(env.action_space.n)
+LEARNING_RATE = 0.1
 
-DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
-DISCRETE_OS_WINDOW_SIZE = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
+DISCOUNT = 0.95
+EPISODES = 25000
 
-print(DISCRETE_OS_WINDOW_SIZE)
+DISCRETE_OS_SIZE = [20, 20]
+discrete_os_win_size = (env.observation_space.high - env.observation_space.low)/DISCRETE_OS_SIZE
+
 
 q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
 
-print(q_table.shape)
-print(q_table)
 
-# done = False
+def get_discrete_state(state):
+    discrete_state = (state - env.observation_space.low)/discrete_os_win_size
+    return tuple(discrete_state.astype(np.int))  # we use this tuple to look up the 3 Q values for the available actions in the q-table
 
-# while not done:
-#     action = 2
-#     new_state, reward, done, truncated, info = env.step(action)
-#     env.render()
-#     print("action: {}, reward: {}".format(action, reward))
 
-# env.close()
-
-# pushing from Predator
+done = False
+while not done:
+    action = 2
+    new_state, reward, done, _ = env.step(action)
+    print(reward, new_state)
+    env.render()
+    #new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
