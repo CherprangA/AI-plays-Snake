b7a6111b3bf6f2af5b4d2b63ccaac22519da18c1
Cherprang <CherprangA@users.noreply.github.com>
2025-04-21 17:02:56 +0900

    cleaning up files

--- archives/first_attempt/RL_agent.py
+++
@@ -1,8 +0,0 @@
-# Define the SimpleAgent class (same as in train_and_save.py)
-class SimpleAgent:
-    def __init__(self, num_actions):
-        self.num_actions = num_actions
-
-    def predict(self, observation):
-        import random
-        return random.randint(0, self.num_actions - 1)
